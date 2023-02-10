5e97e838bcd22e356866a5e0c346f1f28c979b09
Agustin Kassis <webmaster@masize.com>
2023-02-10 13:56:40 -0300

    added subdomain logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-window.location = import.meta.env.VITE_DESTINATION;
+import subdomains from "./config/subdomains.json";
+
+// Get current subdomain
+const subdomain = window.location.host.split(".")[0];
+
+// Match subdomain with config
+const match = subdomains[subdomain as keyof typeof subdomains];
+if (match) {
+  window.location.href = match;
+} else {
+  window.location.href = import.meta.env.VITE_DESTINATION;
+}
