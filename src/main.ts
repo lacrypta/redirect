fb3cecf6f3318233dd25dbc3863d603360b1b561
Agustin Kassis <webmaster@masize.com>
2023-02-10 20:48:15 -0300

    added event tracking

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+declare var gtag: any;
+
+// Import subdomain config
 import subdomains from "./config/subdomains.json";
 
 // Get current subdomain
@@ -5,8 +8,13 @@ const subdomain = window.location.host.split(".")[0];
 
 // Match subdomain with config
 const match = subdomains[subdomain as keyof typeof subdomains];
-if (match) {
-  window.location.href = match;
-} else {
-  window.location.href = import.meta.env.VITE_DESTINATION;
-}
+const href = match ? match : import.meta.env.VITE_DESTINATION;
+
+// Tracks event
+gtag("event", "redirect", {
+  subdomain: match ? subdomain : false,
+  destination: href,
+});
+
+// Redirects
+window.location.href = href;
